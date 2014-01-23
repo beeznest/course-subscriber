a04356e5173bef4a0996935d2422220b0f200c5e
César Perales <cesar.perales@beeznest.com>
2014-01-23 03:49:14 -0500

    logict to show kids or adults modules

--- src/Application/Sonata/UserBundle/Controller/ModulesController.php
+++ src/Application/Sonata/UserBundle/Controller/ModulesController.php
@@ -2,17 +2,35 @@
 
 namespace Application\Sonata\UserBundle\Controller;
 
-use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContext;
+use \Symfony\Component\DependencyInjection\ContainerInterface;
 
 
 /**
  * Extends FOS user bundle security controller.
  */
 
-class ModulesController extends ContainerAware
+class ModulesController extends Controller
 {
+    /**
+     * @return RedirectResponse
+     */
     public function indexAction()
     {
-        return $this->container->get('templating')->renderResponse('ApplicationSonataUserBundle::modules.html.twig');
+        $usr = $this->get('security.context')->getToken()->getUser();
+        $adult = $usr->getAdult();
+        //$class = ($adult) ? 'kids' : 'adult';
+        //$modules = array();
+        error_log(print_r($adult,1));
+
+        if ($adult) {
+            return $this->container->get('templating')->renderResponse('ApplicationSonataUserBundle::modules_kids.html.twig');
+        } else {
+            return $this->container->get('templating')->renderResponse('ApplicationSonataUserBundle::modules_kids.html.twig');
+        }
     }
 }
